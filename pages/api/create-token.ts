6a7472994825b9aed31d31c031ba7fa24d5d5ef2
Cold Code <dee_femifose@yahoo.com>
2023-03-30 17:01:16 -0500

    made creation token better

--- pages/api/create-token.ts
+++ pages/api/create-token.ts
@@ -7,37 +7,47 @@ type Data = {
   response: string;
 };
 type TokenResult = {
+  room: String;
   identity: string;
+  canPublish: boolean;
   accessToken: string;
 };
 
 export default function createToken(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    //    Publishing is true when user is sharing sharing video and audio also
-    const { roomName, participantName, publishing = false } = req.body;
-    console.log(roomName, participantName);
-    const at = new AccessToken(
-      process.env.LIVEKIT_API_KEY,
-      process.env.LIVEKIT_API_SECRET,
-      {
+    try {
+      //    Publishing is true when user is sharing sharing video and audio also
+      const { roomName, participantName, publishing = false } = req.body;
+      console.log(roomName, participantName);
+
+      const at = new AccessToken(
+        process.env.LIVEKIT_API_KEY,
+        process.env.LIVEKIT_API_SECRET,
+        {
+          identity: participantName,
+        }
+      );
+
+      at.addGrant({
+        roomJoin: true,
+        room: roomName,
+        canPublish: publishing,
+        canSubscribe: true,
+      });
+
+      const token = at.toJwt();
+      const result: TokenResult = {
+        room: roomName,
+
         identity: participantName,
-      }
-    );
-
-    at.addGrant({
-      roomJoin: true,
-      room: roomName,
-      canPublish: publishing,
-      canSubscribe: true,
-    });
-
-    const token = at.toJwt();
-    const result: TokenResult = {
-      identity: participantName,
-      accessToken: token,
-    };
-    // console.log('access token', token);
-    res.status(200).json(result);
+        canPublish: publishing,
+        accessToken: token,
+      };
+      res.status(200).json(result);
+    } catch (e) {
+      res.statusMessage = (e as Error).message;
+      res.status(500).end();
+    }
   } else {
     res.status(200).json({ response: "Use POST" });
   }
