28b435ba2c25c5658e7d676261f694ee918a3106
Cold Code <dee_femifose@yahoo.com>
2023-03-30 19:02:40 -0500

    adding dependencies and vendor

---
+++ go.mod
@@ -0,0 +1,22 @@
+module gen
+
+go 1.19
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/livekit/protocol v1.5.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/twitchtv/twirp v8.1.3+incompatible // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/genproto v0.0.0-20230227214838-9b19f0bdc514 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,133 +1,95 @@
 package main
 
 import (
-	"errors"
-	"flag"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
+	"time"
 
-	"github.com/pion/rtp/codecs"
-	"github.com/pion/webrtc/v3"
-	"github.com/pion/webrtc/v3/pkg/media"
-	"github.com/pion/webrtc/v3/pkg/media/h264writer"
-	"github.com/pion/webrtc/v3/pkg/media/ivfwriter"
-	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
-
-	lksdk "github.com/livekit/server-sdk-go"
-	"github.com/livekit/server-sdk-go/pkg/samplebuilder"
+	"github.com/joho/godotenv"
+	auth "github.com/livekit/protocol/auth"
 )
 
-var (
-	host, apiKey, apiSecret, roomName, identity string
-)
+var ApiKey = ""
+var ApiSecret = ""
 
-func init() {
-	flag.StringVar(&host, "host", "", "livekit server host")
-	flag.StringVar(&apiKey, "api-key", "", "livekit api key")
-	flag.StringVar(&apiSecret, "api-secret", "", "livekit api secret")
-	flag.StringVar(&roomName, "room-name", "", "room name")
-	flag.StringVar(&identity, "identity", "", "participant identity")
+type EndpointRequest struct {
+	RoomName        string `json:"roomName"`
+	ParticipantName string `json:"participant"`
+	Publishing      bool   `json:"publishing,omitempty"`
+}
+
+type EndpointResponse struct {
+	Token string `json:"token"`
+}
+
+func generateToken(apiKey string, apiSecret string, room string, identity string, canPublish bool) (string, error) {
+	at := auth.NewAccessToken(apiKey, apiSecret)
+	grant := &auth.VideoGrant{
+		RoomJoin: true,
+		Room:     room,
+	}
+	at.AddGrant(grant).
+		SetIdentity(identity).
+		SetValidFor(time.Hour)
+	return at.ToJWT()
+}
+
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if r.Method != "POST" && r.Method != "OPTIONS" {
+		http.Error(w, "Unsupported method. Please check.", http.StatusNotFound)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var request EndpointRequest
+	err := decoder.Decode(&request)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var canPub bool
+	if request.Publishing {
+		canPub = true
+	} else {
+		canPub = false
+	}
+	token, error := generateToken(ApiKey, ApiSecret, request.RoomName, request.ParticipantName, canPub)
+	if error != nil {
+		log.Fatal(error)
+	}
+	response := EndpointResponse{Token: token}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-	flag.Parse()
-	if host == "" || apiKey == "" || apiSecret == "" || roomName == "" || identity == "" {
-		fmt.Println("invalid arguments.")
-		return
-	}
-	room, err := lksdk.ConnectToRoom(host, lksdk.ConnectInfo{
-		APIKey:              apiKey,
-		APISecret:           apiSecret,
-		RoomName:            roomName,
-		ParticipantIdentity: identity,
-	}, &lksdk.RoomCallback{
-		ParticipantCallback: lksdk.ParticipantCallback{
-			OnTrackSubscribed: onTrackSubscribed,
-		},
-	})
+
+	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		log.Fatal("Error loading .env file")
 	}
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT)
+	// Access environment variables
+	ApiKey = os.Getenv("LIVEKIT_API_KEY")
+	ApiSecret = os.Getenv("LIVEKIT_API_SECRET")
 
-	<-sigChan
-	room.Disconnect()
-}
+	http.HandleFunc("/token", tokenHandler)
+	fmt.Printf("Starting server at port 8082\n")
 
-func onTrackSubscribed(track *webrtc.TrackRemote, publication *lksdk.RemoteTrackPublication, rp *lksdk.RemoteParticipant) {
-	fileName := fmt.Sprintf("%s-%s", rp.Identity(), track.ID())
-	fmt.Println("write track to file ", fileName)
-	NewTrackWriter(track, rp.WritePLI, fileName)
-}
-
-const (
-	maxVideoLate = 1000 // nearly 2s for fhd video
-	maxAudioLate = 200  // 4s for audio
-)
-
-type TrackWriter struct {
-	sb     *samplebuilder.SampleBuilder
-	writer media.Writer
-	track  *webrtc.TrackRemote
-}
-
-func NewTrackWriter(track *webrtc.TrackRemote, pliWriter lksdk.PLIWriter, fileName string) (*TrackWriter, error) {
-	var (
-		sb     *samplebuilder.SampleBuilder
-		writer media.Writer
-		err    error
-	)
-	switch {
-	case strings.EqualFold(track.Codec().MimeType, "video/vp8"):
-		sb = samplebuilder.New(maxVideoLate, &codecs.VP8Packet{}, track.Codec().ClockRate, samplebuilder.WithPacketDroppedHandler(func() {
-			pliWriter(track.SSRC())
-		}))
-		// ivfwriter use frame count as PTS, that might cause video played in a incorrect framerate(fast or slow)
-		writer, err = ivfwriter.New(fileName + ".ivf")
-
-	case strings.EqualFold(track.Codec().MimeType, "video/h264"):
-		sb = samplebuilder.New(maxVideoLate, &codecs.H264Packet{}, track.Codec().ClockRate, samplebuilder.WithPacketDroppedHandler(func() {
-			pliWriter(track.SSRC())
-		}))
-		writer, err = h264writer.New(fileName + ".h264")
-
-	case strings.EqualFold(track.Codec().MimeType, "audio/opus"):
-		sb = samplebuilder.New(maxAudioLate, &codecs.OpusPacket{}, track.Codec().ClockRate)
-		writer, err = oggwriter.New(fileName+".ogg", 48000, track.Codec().Channels)
-
-	default:
-		return nil, errors.New("unsupported codec type")
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	t := &TrackWriter{
-		sb:     sb,
-		writer: writer,
-		track:  track,
-	}
-	go t.start()
-	return t, nil
-}
-
-func (t *TrackWriter) start() {
-	defer t.writer.Close()
-	for {
-		pkt, _, err := t.track.ReadRTP()
-		if err != nil {
-			break
-		}
-		t.sb.Push(pkt)
-
-		for _, p := range t.sb.PopPackets() {
-			t.writer.WriteRTP(p)
-		}
+	if err := http.ListenAndServe(":8082", nil); err != nil {
+		log.Fatal(err)
 	}
 }
